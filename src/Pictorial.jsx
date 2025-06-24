9b891b4996b85fa70337821e7192e8ee86283082
Tony Stark <tonystark950@myyahoo.com>
2025-06-24 12:17:45 -0400

    Add files via upload
    pictorial java script file

---
+++ src/Pictorial.jsx
@@ -0,0 +1,13 @@
+// src/Pictorial.jsx
+import React from 'react';
+import './Pictorial.css'; // Styling will go here
+
+function Pictorial() {
+  return (
+    <div className="pictorial-container">
+      <img src="/labrador-retriever-.jpg" alt="Restaurant" className="pictorial-img" />
+    </div>
+  );
+}
+
+export default Pictorial;
